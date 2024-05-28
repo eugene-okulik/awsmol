702b45663f11dae9b4acc2a96bcef6f569186052
sasha <awsmol@mail.ru>
2024-05-28 21:15:21 +0300

    homework_3 done

---
+++ homework/aleksandra_smol/homework_3/task_1.py
@@ -0,0 +1,13 @@
+a = 5
+b = 3
+
+summa = a + b
+print(summa)
+
+raznost = a - b
+print(raznost)
+
+proizvedenie = a * b
+print(proizvedenie)
+
+
---
+++ homework/aleksandra_smol/homework_3/task_2.py
@@ -0,0 +1,5 @@
+x = 2
+y = 4
+
+primer = x - y / 1 + x * y
+print(primer)
